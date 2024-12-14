91d6087bb518f7cb873382c3fbcb8b37c64ed4bc
Arisha <selestinatink@mail.ru>
2024-12-14 17:19:36 +0300

    second option

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,9 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        IUStudent student1 = new IUStudent("Vika","Tsyganova", "yuriosen", "CS",5);
-        IUStudent student2 = new IUStudent("Arina","Ivantsova", "Tinki", "CS",5);
-        MathStudent student3 = new MathStudent("Andre","Zimmer", "TapsieSpot", "Mathematics",2);
+        Student student1 = new IUStudent("Vika","Tsyganova", "yuriosen", "CS",5);
+        Student student2 = new IUStudent("Arina","Ivantsova", "Tinki", "CS",5);
+        Student student3 = new MathStudent("Andre","Zimmer", "TapsieSpot", "Mathematics",2);
 // Информация об экзаменах
         student1.writeExam();
         student2.writeExam();
